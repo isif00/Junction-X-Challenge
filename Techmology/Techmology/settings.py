1852bd27d2853f5c8d2bac306999fb1500314a0e
isif00 <sifodev@gmail.com>
2023-03-11 01:58:42 +0100

    Fixing Security issues

--- Techmology/Techmology/settings.py
+++ Techmology/Techmology/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m9t*1abk26m*$mpl$h48j252p&61n33lwnbwfywm1)$b24x3es'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
